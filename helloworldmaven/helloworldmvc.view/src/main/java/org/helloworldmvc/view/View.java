55de2de033531b7982f75b872eea2a640a4e58e1
user <user@10.64.128.104>
2018-04-27 14:30:15 +0200

    phase4

--- helloworldmaven/helloworldmvc.view/src/main/java/org/helloworldmvc/view/View.java
+++ helloworldmaven/helloworldmvc.view/src/main/java/org/helloworldmvc/view/View.java
@@ -2,7 +2,7 @@ package org.helloworldmvc.view;
 
 import org.helloworldmvc.contract.IView;
 
-public class View implements IView {
+public class View {
 	
 	public String displayMessage(String message) {
 		return message;
