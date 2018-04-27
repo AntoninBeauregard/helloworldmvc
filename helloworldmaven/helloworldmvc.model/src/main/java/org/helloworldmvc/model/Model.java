31e78b52380ad8e661290e3dc3822db284e346cf
user <user@10.64.128.104>
2018-04-27 09:16:53 +0200

    Phase 3

--- helloworldmaven/helloworldmvc.model/src/main/java/org/helloworldmvc/model/Model.java
+++ helloworldmaven/helloworldmvc.model/src/main/java/org/helloworldmvc/model/Model.java
@@ -1,10 +1,26 @@
 package org.helloworldmvc.model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import org.helloworldmvc.contract.IModel;
 
 public class Model implements IModel{
-	public String getHelloWorld() {
-		return null;
+	public String getHelloWorld1() {
+	    return null;
 	}
-
+	private static final String CHEMIN = "helloFile.txt"; //Emplacmeent du fichier
+	
+	public String getHelloWorld() {
+	    // TODO Auto-generated method stub
+	    String r = "[ERREUR E/S]"; //Sera changé si tout se passe correctement
+	    try {
+	        BufferedReader bf = new BufferedReader(new FileReader(CHEMIN)); //Créaction du lecteur
+	        r = bf.readLine(); //On lit
+	        bf.close(); //On ferme
+	    } catch (Exception e) /*S'il y a une couille*/ {
+	        e.printStackTrace(); //Vomir l'erreur à l'utilisateur
+	    }
+	    return r; //Mange ton String toi qui m'a appelé
+	}
 }
